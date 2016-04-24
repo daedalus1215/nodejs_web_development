ea8f6da15f1cfbbba0dbe67b1084be133d96a96c
ladams@yahoo.com <ladams@yahoo.com>
2016-04-24 10:17:11 -0400

    faker demo

--- myShop/listProducts.js
+++ myShop/listProducts.js
@@ -1,4 +1,15 @@
 var faker = require('../node_modules/faker');
-var randomName = faker.name.firstName();
 
-console.log(randomName);
+var products = new Array();
+
+for (var i = 0; i < 10; i++) {
+  // build up our product.
+  var product = faker.commerce.productName();
+  var price = faker.commerce.price();
+  var productPrice = product + " - $" + price;
+  products.push(productPrice);
+}
+
+products.forEach (function(product) {
+  console.log(product);
+}); 
