b6190e701fafe3f0dbf194857945ca4d8fcc31af
ladams <ladams@sweetser.org>
2016-04-25 11:21:23 -0400

    successfully used ejs to create a basic dynamic html page

--- moreExpress/index.js
+++ moreExpress/index.js
@@ -9,7 +9,12 @@ var app = express();
 
 app.get('/', function(req, res) {
   console.log("working");
-  res.send("Working");
+  res.render("home.ejs");
+});
+
+app.get('/fallin/:thing', function(req, res) {
+  var thing = req.params.thing;
+  res.render("love.ejs", {thingVar: thing});
 });
 
 
