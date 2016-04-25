91d6c6bdea1852b071749e961fbdb9a13366da17
ladams <ladams@sweetser.org>
2016-04-25 10:11:34 -0400

    worked on the * route matcher and parameters in a url for nodejs

--- expressApp/app.js
+++ expressApp/app.js
@@ -13,8 +13,25 @@ app.get('/dog', function(req, res) {
   res.send("Dog Page");
 });
 
+/* This is routes for url patterns. - /r/<anything> */
+app.get("/r/:subredditName", function(req, res) {
+  console.log(req.params.subredditName); // this will give us the actual value put in at /r/<anything>
+  var subredit = req.params.subredditName;
+  res.send("Welcome to the "+ subredit.toUpperCase() +" subredit.");
+});
+
+// url pattern for /r/<anything>/comments/<anything>/<anything>
+app.get("/r/:subredditName/comments/:id/:title/", function(req, res) {
+  res.send("welcome to the comments page!");
+});
+
+
+// This will cover all url requests other than the ones we already defined.
 app.get('*', function(req, res) {
   res.send("You are a star.");
 });
+
+
+
 
 app.listen(3000);
