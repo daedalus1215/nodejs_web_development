9b4e54a3dd152eeeb12c755254d0d7a751185061
ladams1776@yahoo.com <ladams1776@yahoo.com>
2016-04-26 13:14:16 -0400

    YelpCamp route setup

--- YelpCamp/v1/app.js
+++ YelpCamp/v1/app.js
@@ -13,9 +13,13 @@ app.get("/", function(req, res) {
 
 app.get("/campgrounds", function(req, res) {
   var campgrounds = [
-    { name: "Augustus Camp", title: "http://" },
-    { name: "Nietzsche Camp", title: "http://" }
+    { name: "Augustus Camp", image: "https://farm4.staticflickr.com/3795/10131087094_c1c0a1c859.jpg" },
+    { name: "Nietzsche Camp", image: "https://farm3.staticflickr.com/2464/3694344957_14180103ed.jpg" }, 
+    { name: "Confuscious Camp", image: "https://farm5.staticflickr.com/4016/4369518024_0f64300987.jpg" }
   ];
+  
+  res.render("campgrounds", {campgrounds, campgrounds});
+  
 });
 
 
