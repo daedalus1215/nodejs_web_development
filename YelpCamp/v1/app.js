cf3d01f369fa1f5fb9c919059c252c5df9b5a6c4
ladams1776@yahoo.com <ladams1776@yahoo.com>
2016-04-26 13:34:30 -0400

    post campgrounds route now will add a new campground to our array and redirects us back to campgrounds

--- YelpCamp/v1/app.js
+++ YelpCamp/v1/app.js
@@ -11,16 +11,38 @@ app.get("/", function(req, res) {
 });
 
 
-app.get("/campgrounds", function(req, res) {
-  var campgrounds = [
+
+
+
+
+
+/************************************************* beg - CAMPGROUNDS *******************************************************************************************************/
+var campgrounds = 
+  [
     { name: "Augustus Camp", image: "https://farm4.staticflickr.com/3795/10131087094_c1c0a1c859.jpg" },
     { name: "Nietzsche Camp", image: "https://farm3.staticflickr.com/2464/3694344957_14180103ed.jpg" }, 
     { name: "Confuscious Camp", image: "https://farm5.staticflickr.com/4016/4369518024_0f64300987.jpg" }
   ];
-  
+
+app.get("/campgrounds", function(req, res) {
   res.render("campgrounds", {campgrounds, campgrounds});
-  
 });
+
+app.get("/campgrounds/new", function(req, res) {
+  res.render("new")
+});
+
+app.post("/campgrounds", function(req, res) {
+  // Get data from form and add to campgrounds array.
+  var name = req.body.name;
+  var img = req.body.img;
+  var newCampground = {name: name, image: img};
+  campgrounds.push(newCampground);
+  // Redirect us back to campgrounds.
+  res.redirect("/campgrounds");
+});
+
+/************************************************* end - CAMPGROUNDS *******************************************************************************************************/
 
 
 app.listen(3000, function() {
