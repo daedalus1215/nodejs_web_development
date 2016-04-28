e413498ac1a996cab34dab4e969fcc0bc7622bd4
ladams1776@yahoo.com <ladams1776@yahoo.com>
2016-04-28 13:15:23 -0400

    Integrated mongoDB with new and existing campgrounds

--- YelpCamp/v2/app.js
+++ YelpCamp/v2/app.js
@@ -1,12 +1,16 @@
-var express = require("express");
-var mongoClient = require('mongodb');
+var express = require("express"),
+    mongoClient = require('mongodb'),
+    mongoose = require('mongoose'),
+    app = express(),
+    bodyParser = require("body-parser");
 
-var app = express();
-var bodyParser = require("body-parser");
+mongoose.connect("mongodb://localhost/yelp_camp"); // Must connect our ORM to the db.
 
-// Connect to the db
-mongoClient.connect
-
+// Establish a Schema
+var campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String
+});
 
 
 app.use(bodyParser.urlencoded({ extended : true})) // make sure that we are using the body parser and setting extended option to true.
@@ -18,25 +22,64 @@ app.get("/", function(req, res) {
 });
 
 
-
-
-
-
-
 /************************************************* beg - CAMPGROUNDS *******************************************************************************************************/
-var campgrounds = 
-  [
-    { name: "Augustus Camp", image: "https://farm4.staticflickr.com/3795/10131087094_c1c0a1c859.jpg" },
-    { name: "Nietzsche Camp", image: "https://farm3.staticflickr.com/2464/3694344957_14180103ed.jpg" }, 
-    { name: "Confuscious Camp", image: "https://farm5.staticflickr.com/4016/4369518024_0f64300987.jpg" },
-    { name: "Confuscious Camp", image: "https://farm5.staticflickr.com/4016/4369518024_0f64300987.jpg" },
-    { name: "Confuscious Camp", image: "https://farm5.staticflickr.com/4016/4369518024_0f64300987.jpg" },
-    { name: "Confuscious Camp", image: "https://farm5.staticflickr.com/4016/4369518024_0f64300987.jpg" },
-    { name: "Confuscious Camp", image: "https://farm5.staticflickr.com/4016/4369518024_0f64300987.jpg" }
-  ];
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+Campground.create(
+  { 
+    name: "Augustus Camp", 
+    image: "https://farm4.staticflickr.com/3795/10131087094_c1c0a1c859.jpg" 
+  }
+  , function(err, camp) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("NEWLY CREATED CAMPGROUND: ");
+        console.log(camp);
+      }
+});
+
+
+Campground.create(
+  { name: "Nietzsche Camp", image: "https://farm3.staticflickr.com/2464/3694344957_14180103ed.jpg" }
+  , function(err, camp) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("NEWLY CREATED CAMPGROUND: ");
+        console.log(camp);
+      }
+});
+Campground.create(
+   { name: "Confuscious Camp", image: "https://farm5.staticflickr.com/4016/4369518024_0f64300987.jpg" }
+  , function(err, camp) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("NEWLY CREATED CAMPGROUND: ");
+        console.log(camp);
+      }
+});
+Campground.create(
+   { name: "Confuscious Camp", image: "https://farm5.staticflickr.com/4016/4369518024_0f64300987.jpg" }
+  , function(err, camp) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("NEWLY CREATED CAMPGROUND: ");
+        console.log(camp);
+      }
+});
 
 app.get("/campgrounds", function(req, res) {
-  res.render("campgrounds", {campgrounds, campgrounds});
+  // Get all campgrounds from db
+  Campground.find({}, function(err, campgrounds) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("campgrounds", {campgrounds, campgrounds});
+    }
+  });
 });
 
 app.get("/campgrounds/new", function(req, res) {
@@ -48,9 +91,16 @@ app.post("/campgrounds", function(req, res) {
   var name = req.body.name;
   var img = req.body.img;
   var newCampground = {name: name, image: img};
-  campgrounds.push(newCampground);
-  // Redirect us back to campgrounds.
-  res.redirect("/campgrounds");
+  
+  Campground.create(newCampground, function(err, newCamp) {
+    if (err) {
+      console.log(err);
+    } else {
+      // Redirect us back to campgrounds.
+      res.redirect("/campgrounds");
+    }
+  });
+  
 });
 
 /************************************************* end - CAMPGROUNDS *******************************************************************************************************/
