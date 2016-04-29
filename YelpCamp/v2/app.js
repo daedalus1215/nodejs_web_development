bef2b8ef208000201da640aa4ef642ef8ba81b8f
Larry <ladams1776@yahoo.com>
2016-04-29 17:10:33 -0400

    descriptions now carry over for individual campgrounds and for editing

--- YelpCamp/v2/app.js
+++ YelpCamp/v2/app.js
@@ -35,7 +35,8 @@ app.get('/', function(req, res) {
 //Campground.create(
 //  { 
 //    name: 'Augustus Camp', 
-//    image: 'https://farm4.staticflickr.com/3795/10131087094_c1c0a1c859.jpg' 
+//    image: 'https://farm4.staticflickr.com/3795/10131087094_c1c0a1c859.jpg',
+//    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum'
 //  }
 //  , function(err, camp) {
 //      if (err) {
@@ -48,7 +49,11 @@ app.get('/', function(req, res) {
 //
 //
 //Campground.create(
-//  { name: 'Nietzsche Camp', image: 'https://farm3.staticflickr.com/2464/3694344957_14180103ed.jpg' }
+//  { 
+//    name: 'Nietzsche Camp', 
+//    image: 'https://farm3.staticflickr.com/2464/3694344957_14180103ed.jpg',
+//    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum'
+//  }
 //  , function(err, camp) {
 //      if (err) {
 //        console.log(err);
@@ -58,7 +63,11 @@ app.get('/', function(req, res) {
 //      }
 //});
 //Campground.create(
-//   { name: 'Confuscious Camp', image: 'https://farm5.staticflickr.com/4016/4369518024_0f64300987.jpg' }
+//   { 
+//    name: 'Confuscious Camp', 
+//    image: 'https://farm5.staticflickr.com/4016/4369518024_0f64300987.jpg',
+//    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum'
+//   }
 //  , function(err, camp) {
 //      if (err) {
 //        console.log(err);
@@ -141,7 +150,6 @@ app.get('/campgrounds/:id/edit', function(req, res) {
       res.render('edit', {campground: foundCampground});
     }
   });
- //res.render('edit');
 });
 
 //UPDATE ROUTE
