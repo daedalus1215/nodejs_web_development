50fa80ce194c7c773c433dc6f212b087e9b6f170
ladams1776@yahoo.com <ladams1776@yahoo.com>
2016-04-29 10:32:07 -0400

    Translating POST to PUT to stay respectful of RESTful design. Began to work on the edit pages of campgrounds

--- YelpCamp/v2/app.js
+++ YelpCamp/v2/app.js
@@ -1,27 +1,31 @@
-var express = require("express"),
-    mongoClient = require('mongodb'),
-    mongoose = require('mongoose'),
-    app = express(),
-    bodyParser = require("body-parser");
+var express         = require('express'),
+    mongoClient     = require('mongodb'),
+    mongoose        = require('mongoose'),
+    methodOverride  = require('method-override'),
+    app             = express(),
+    bodyParser      = require('body-parser');
 
-mongoose.connect("mongodb://localhost/yelp_camp"); // Must connect our ORM to the db.
 
-// Establish a Schema
+// APP CONFIG
+mongoose.connect('mongodb://localhost/yelp_camp'); // Must connect our ORM to the db.
+app.use(bodyParser.urlencoded({extended : true})) // make sure that we are using the body parser and setting extended option to true.
+app.set('view engine', 'ejs'); // Set the view engine to be ejs.
+app.use(methodOverride('_method')); // Tell out app that whenever we get the request that has _method as a parameter, take whatever it is equal to and treat that request as a put request or as a delete request.
+
+
+// MONGOOSE/MODEL CONFIG
 var campgroundSchema = new mongoose.Schema({
   name: String,
   image: String,
   description: String
 });
+var Campground = mongoose.model('Campground', campgroundSchema); 
 
-var Campground = mongoose.model("Campground", campgroundSchema); 
 
-app.use(bodyParser.urlencoded({ extended : true})) // make sure that we are using the body parser and setting extended option to true.
-
-app.set("view engine", "ejs"); // Set the view engine to be ejs.
 
 //INDEX - Homepage.
-app.get("/", function(req, res) {
-  res.render("landing");
+app.get('/', function(req, res) {
+  res.render('landing');
 });
 
 
@@ -30,73 +34,73 @@ app.get("/", function(req, res) {
 
 //Campground.create(
 //  { 
-//    name: "Augustus Camp", 
-//    image: "https://farm4.staticflickr.com/3795/10131087094_c1c0a1c859.jpg" 
+//    name: 'Augustus Camp', 
+//    image: 'https://farm4.staticflickr.com/3795/10131087094_c1c0a1c859.jpg' 
 //  }
 //  , function(err, camp) {
 //      if (err) {
 //        console.log(err);
 //      } else {
-//        console.log("NEWLY CREATED CAMPGROUND: ");
+//        console.log('NEWLY CREATED CAMPGROUND: ');
 //        console.log(camp);
 //      }
 //});
 //
 //
 //Campground.create(
-//  { name: "Nietzsche Camp", image: "https://farm3.staticflickr.com/2464/3694344957_14180103ed.jpg" }
+//  { name: 'Nietzsche Camp', image: 'https://farm3.staticflickr.com/2464/3694344957_14180103ed.jpg' }
 //  , function(err, camp) {
 //      if (err) {
 //        console.log(err);
 //      } else {
-//        console.log("NEWLY CREATED CAMPGROUND: ");
+//        console.log('NEWLY CREATED CAMPGROUND: ');
 //        console.log(camp);
 //      }
 //});
 //Campground.create(
-//   { name: "Confuscious Camp", image: "https://farm5.staticflickr.com/4016/4369518024_0f64300987.jpg" }
+//   { name: 'Confuscious Camp', image: 'https://farm5.staticflickr.com/4016/4369518024_0f64300987.jpg' }
 //  , function(err, camp) {
 //      if (err) {
 //        console.log(err);
 //      } else {
-//        console.log("NEWLY CREATED CAMPGROUND: ");
+//        console.log('NEWLY CREATED CAMPGROUND: ');
 //        console.log(camp);
 //      }
 //});
 //Campground.create(
 //   {
-//    name: "Confuscious Camp", 
-//    image: "https://farm5.staticflickr.com/4016/4369518024_0f64300987.jpg",
-//    description: "This is a huge granite hill, no bathrooms. No water. Beautiful granite!"
+//    name: 'Confuscious Camp', 
+//    image: 'https://farm5.staticflickr.com/4016/4369518024_0f64300987.jpg',
+//    description: 'This is a huge granite hill, no bathrooms. No water. Beautiful granite!'
 //   }
 //  , function(err, camp) {
 //      if (err) {
 //        console.log(err);
 //      } else {
-//        console.log("NEWLY CREATED CAMPGROUND: ");
+//        console.log('NEWLY CREATED CAMPGROUND: ');
 //        console.log(camp);
 //      }
 //});
 
-//INDEX - display all campgrounds
-app.get("/campgrounds", function(req, res) {
+//INDEX ROUTE - display all campgrounds
+app.get('/campgrounds', function(req, res) {
   // Get all campgrounds from db
   Campground.find({}, function(err, campgrounds) {
     if (err) {
       console.log(err);
     } else {
-      res.render("campgrounds", {campgrounds, campgrounds});
+      res.render('campgrounds', {campgrounds, campgrounds});
     }
   });
 });
 
-//NEW - show form to create new campground
-app.get("/campgrounds/new", function(req, res) {
-  res.render("new")
+//NEW ROUTE - show form to create new campground
+app.get('/campgrounds/new', function(req, res) {
+  res.render('new')
 });
 
-//CREATE - add new campground to DB
-app.post("/campgrounds", function(req, res) {
+//CREATE ROUTE - add new campground to DB
+app.post('/campgrounds', function(req, res) {
   // Get data from form and add to campgrounds array.
   var name = req.body.name;
   var img = req.body.img;
@@ -107,23 +111,39 @@ app.post("/campgrounds", function(req, res) {
       console.log(err);
     } else {
       // Redirect us back to campgrounds.
-      res.redirect("/campgrounds");
+      res.redirect('/campgrounds');
     }
   });
   
 });
 
-//SHOW - display info about one campground.
-app.get("/campgrounds/:id", function(req, res) {
+//SHOW ROUTE - display info about one campground.
+app.get('/campgrounds/:id', function(req, res) {
   //find the campground with provided ID
   //render show template with that campground.
-  res.render("show");
+  res.render('show');
 });
 
+//EDIT ROUTE - edit the campground
+app.get('/campgrounds/:id/edit', function(req, res) {
+  Campground.findById(req.params.id, function(err, foundCampground) {
+    if (err) {
+      console.log('Error with the campground retrieval');
+      console.log(err);      
+    } else {
+      res.render('edit', {campground: foundCampground});
+    }
+  });
+ //res.render('edit');
+});
 
+//UPDATE ROUTE
+//app.put('/campgrounds/:id', function(req, res) {
+//  
+//});
 /************************************************* end - CAMPGROUNDS ****************************************************************/
 
 
 app.listen(3000, function() {
-  console.log("YelpCamp Server has started");
+  console.log('YelpCamp Server has started');
 });
