4d0aab89625af2736871d0171f136d43ed718feb
ladams1776@yahoo.com <ladams1776@yahoo.com>
2016-05-03 10:52:13 -0400

    We now have the ability to register and it does save in the db, then we also handle sessions.

--- authentication_demo/app.js
+++ authentication_demo/app.js
@@ -15,6 +15,8 @@ var app = express();
 // Setup the View Engine (engine we are using to render pages).
 app.set('view engine', 'ejs');
 
+// Make sure we use bodyParser when handling a form.
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(passport.initialize()); // tell app to use passport
 app.use(passport.session()); // tell app to setup session
@@ -32,8 +34,9 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 
-
-
+//==========================
+// ROUTES
+//==========================
 
 app.get('/', function(req, res) {
   res.render('home');
@@ -42,6 +45,48 @@ app.get('/', function(req, res) {
 app.get('/secret', function(req, res) {
   res.render('secret');
 })
+
+
+// Auth Routes
+
+// show signup form
+app.get('/register', function(req, res) {
+  res.render('register');
+});
+
+// handle user signup form.
+app.post('/register', function(req, res) {
+  // grab the info from the form
+  var fUsername = req.body.newUser.username;
+  var fPassword = req.body.newUser.password;
+  User.register(new User({username: fUsername}), fPassword, function(err, rUser) {
+    if (err) {
+      console.log("ERROR registering new user - " + err);
+    } else {
+      console.log('registered');
+      return res.render('register');
+    }
+    // log the user in, handle session and use serializeUser - we are declaring we are using the "local" strategy.
+    passport.authenticate("local")(req, res, function() {
+      res.redirect('/secret');
+    });
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.listen(3000, function() {
   console.log("Authentication has begun");
