ed43c1ee75f3d56644436d016e74174400f94505
ladams1776@yahoo.com <ladams1776@yahoo.com>
2016-05-03 10:16:40 -0400

    setup of auth

---
+++ authentication_demo/app.js
@@ -0,0 +1,20 @@
+var express = require('express'),
+    app = express();
+
+
+app.set('view engine', 'ejs');
+
+
+
+
+app.get('/', function(req, res) {
+  res.render('home');
+});
+
+app.get('/secret', function(req, res) {
+  res.render('secret');
+})
+
+app.listen(3000, function() {
+  console.log("Authentication has begun");
+});
